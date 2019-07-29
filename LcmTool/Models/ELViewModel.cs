41ce032f9396f7ab8d062344d1a723b4a22e286d
ECASS1 <ECASS1@AMONWIRVLCMPD01>
2019-07-28 19:16:36 -0500

    Added kendo grid to index view

---
+++ LcmTool/Models/ELViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LcmTool.Models
+{
+    public class ELViewModel
+    {
+        public int lcmID { get; set; }
+        public Nullable<int> resourceID { get; set; }
+        public Nullable<int> statusID { get; set; }
+    }
+}
